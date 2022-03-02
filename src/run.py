ee2fde55227d96023dc08affdb552af19bfb8241
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-02 12:52:56 +0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -28,7 +28,8 @@ async def send_welcome(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(f'"{message.text}" не является служебной командой. User_id: {message.chat.id}')
+    await for i in range(10):
+        message.answer(f'"{message.text}" не является служебной командой. User_id: {message.chat.id}')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
