9b6313e57827b43dc846ab00c425d884a89b71c5
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-04 11:08:43 +0500

    Update facts.py

--- src/facts.py
+++ src/facts.py
@@ -1,1 +1,20 @@
+import random
+import mysql.connector
+import yaml
+from os import path as os_path
 
+config_path = os_path.abspath(os_path.join(os_path.dirname(__file__), 'config.yml'))
+data = yaml.safe_load(open(config_path))
+
+def get_fact():
+  mydb = mysql.connector.connect(
+    host=data['DB_HOST'],
+    user=data['DB_USERNAME'],
+    password=data['DB_PASSWORD'],
+    database=data['DB_NAME'])
+  mycursor = mydb.cursor(buffered=True)
+  mycursor.execute(f"SELECT * FROM users WHERE user_id = '{user_id}'")
+  myresult = mycursor.fetchone()
+  mycursor.close()
+  mydb.close()
+  return str(myresult)
