65184f33c4c6688b37f220502e2edc8a7860e2ea
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-04 15:42:21 +0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -23,8 +23,7 @@ async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
     answer_data = query.data
     # always answer callback queries, even if you have nothing to say
     await query.answer(f'You answered with {answer_data!r}')
-    await message.reply(facts.get_fact(), parse_mode='html', reply_markup=inline_fact_button)
-
+    await bot.send_message(query.from_user.id, facts.get_fact(), parse_mode='html', reply_markup=inline_fact_button)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
