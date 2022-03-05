e7c4f84f4c85b4b16a3eb75fd1ccb2538609a576
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-06 00:24:32 +0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -4,6 +4,7 @@ import registration
 import facts
 import yaml
 from os import path as os_path
+import aiomysql
 
 config_path = os_path.abspath(os_path.join(os_path.dirname(__file__), 'config.yml'))
 config = yaml.safe_load(open(config_path))
@@ -22,13 +23,7 @@ dp = Dispatcher(bot)
 @dp.callback_query_handler(text='next')
 async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
     await query.answer()
-    await query.message.edit_text(text=facts.get_fact(query.message.text), reply_markup=inline_fact_button, parse_mode='html')
-    
-@dp.callback_query_handler(text='good')
-@dp.callback_query_handler(text='bad')
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    await query.answer("Спасибо, учтём!")
-    await query.message.edit_reply_markup(reply_markup=inline_fact_button_next)
+    await query.message.edit_text(text=f'csdcsdcsacdsdcasdc', reply_markup=inline_fact_button, parse_mode='html')
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -36,7 +31,8 @@ async def send_welcome(message: types.Message):
     
 @dp.message_handler(commands=['fact'])
 async def send_welcome(message: types.Message):
-    await message.reply(facts.extract_fact(), parse_mode='html', reply_markup=inline_fact_button)
+    mydb = await aiomysql.aiomysql.connect(host=data['DB_HOST'], user=data['DB_USERNAME'], password=data['DB_PASSWORD'], db=data['DB_NAME'])
+    await message.reply('csdcsdcsdcsdc', parse_mode='html', reply_markup=inline_fact_button)
     
 @dp.message_handler(commands=['write'])
 async def send_welcome(message: types.Message):
