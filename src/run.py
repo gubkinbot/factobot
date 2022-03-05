ac2407b7301b850f49c8faf332e602355ee0afb3
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-06 01:14:41 +0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -2,6 +2,7 @@
 import yaml
 from os import path as os_path
 import telebot
+import facts
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 config_path = os_path.abspath(os_path.join(os_path.dirname(__file__), 'config.yml'))
@@ -22,11 +23,11 @@ def send_welcome(message):
 
 @bot.message_handler(commands='fact')
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Факт!", reply_markup=markup)
+    bot.send_message(message.chat.id, facts.extract_fact(), reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     bot.answer_callback_query(call.id, "Едем дальше...")
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text='fdjghjghjfghjfgh', reply_markup=markup)
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=facts.extract_fact(), reply_markup=markup)
 
 bot.infinity_polling()
