4b842cd8f05dbfc0e48d90198bdc6e20c3ba08bc
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-03-05 12:49:24 +0500

    Update facts.py

--- src/facts.py
+++ src/facts.py
@@ -6,12 +6,18 @@ from os import path as os_path
 config_path = os_path.abspath(os_path.join(os_path.dirname(__file__), 'config.yml'))
 data = yaml.safe_load(open(config_path))
 
-def get_fact():
+def get_fact(old_fact):
+  new_fact = extract_fact()
+  while new_fact != old_fact:
+    new_fact = extract_fact()
+  return new_fact
+
+def extract_fact():  
   mydb = mysql.connector.connect(
-    host=data['DB_HOST'],
-    user=data['DB_USERNAME'],
-    password=data['DB_PASSWORD'],
-    database=data['DB_NAME'])
+  host=data['DB_HOST'],
+  user=data['DB_USERNAME'],
+  password=data['DB_PASSWORD'],
+  database=data['DB_NAME'])
 
   mycursor = mydb.cursor(buffered=True)
   mycursor.execute("SELECT * FROM facts ORDER BY RAND() LIMIT 1")
