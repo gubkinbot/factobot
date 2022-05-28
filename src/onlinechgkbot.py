7b8bccf7264e178cbef54830c21178f98810ef67
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2022-05-28 11:00:15 +0500

    Update onlinechgkbot.py

--- src/onlinechgkbot.py
+++ src/onlinechgkbot.py
@@ -8,6 +8,25 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeybo
 import yaml
 from os import path as os_path
 
+mydb = mysql.connector.connect(
+    host=data['DB_HOST'],
+    user=data['DB_USERNAME'],
+    password=data['DB_PASSWORD'],
+    database=data['DB_NAME'])
+mycursor = mydb.cursor(buffered=True)
+mycursor.execute(f"SELECT * FROM `TABLE 1` WHERE `user_id` = {user_id}")
+myresult = mycursor.rowcount
+myresult_data = mycursor.fetchone()
+
+
+@bot.message_handler(content_types=['message'])
+def contact(message):
+    bot.send_message(message.chat.id, f'''<b>Добро пожаловать!</b>''')
+
+
+
+
+
 config_path = os_path.abspath(os_path.join(os_path.dirname(__file__), 'config.yml'))
 data = yaml.safe_load(open(config_path))
 
