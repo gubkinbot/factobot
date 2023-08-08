43cf3653ba7fefff90af77fcd0f92cb29bd2f7d2
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2023-08-08 20:58:47 +0500

    upd!

--- bot/bot.py
+++ bot/bot.py
@@ -21,7 +21,7 @@ def echo_message(message):
     item2 = types.InlineKeyboardButton('👍', callback_data='good')
     markup.add(item1, item2)
     formatted_text = f"Это текст, в котором есть скрытый текст: \n\n ||spoiler|| Скрытый текст:"
-    bot.send_message(chat_id=message.chat.id, text=formatted_text, reply_markup=markup, parse_mode='Markdown')
+    bot.send_message(chat_id=message.chat.id, text=formatted_text, reply_markup=markup, parse_mode='MarkdownV2')
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
