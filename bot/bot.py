4c56c17a911cecb83a7a7c5fdf03f4f4d56581f6
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2023-08-14 02:14:44 +0500

    upd

--- bot/bot.py
+++ bot/bot.py
@@ -68,7 +68,7 @@ def send_fact(message):
     formatted_text = f'''<strong>{fact.iloc[0].note_id}</strong>
 
 {fact.iloc[0].note_text}'''	 
-    pd.DataFrame(columns=['user_id', 'action', 'note_id'], data=[[message.chat.id, 'fact', fact.iloc[0].note_id]]).to_sql(name='log', con=engine, if_exists='append', index=False)
+    pd.DataFrame(columns=['user_id', 'action', 'note_id'], data=[[message.chat.id, '/fact', fact.iloc[0].note_id]]).to_sql(name='log', con=engine, if_exists='append', index=False)
     bot.send_message(chat_id=message.chat.id, text=formatted_text, reply_markup=markup, parse_mode='HTML')
 
 @bot.message_handler(func=lambda message: True)
